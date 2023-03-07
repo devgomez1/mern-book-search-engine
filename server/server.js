645cdebd6089eed3d5aa184687c58d00cc3a5f07
devin <devinelliotgomez@gmail.com>
2023-03-07 16:42:43 -0600

    implemented apollo in server.js(applied it to the express server as middlewear), now going to move on to the schemas directory to configure typedefs and resolvers.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const {apolloServer} = require('apollo-server-express');
-
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,8 +16,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.use(authMiddleware);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    return { user: req.user || null };
+  },
+});
+
+server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
